ceaf13277eeeba6b92861aada66d5062aa626bcc
zameel7 <zameel7@users.noreply.github.com>
2025-02-22 02:14:15 +0000

    deploy: 6578097b75d5fa14c8e2fb337df7dd505a69c684

---
+++ static/js/664.2584c7da.chunk.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[664],{5664:(e,t,r)=>{r.r(t),r.d(t,{default:()=>b});var n=r(7419),i=r(1645),s=r(3614),a=r(9),o=r(2582),c=r(8156),l=r(579);const d=a.Ay.section`
+  padding: 5rem 0;
+  background: rgb(241, 242, 243);
+`,x=a.Ay.h2`
+  text-align: center;
+  margin-bottom: 3rem;
+`,h=(0,a.Ay)((e=>{let{to:t,...r}=e;return t.startsWith("http")?(0,l.jsx)("a",{href:t,target:"_blank",rel:"noopener noreferrer",...r}):(0,l.jsx)(o.N_,{to:t,...r})}))`
+  display: block;
+  background: white;
+  padding: 2rem;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+  text-align: center;
+  height: 100%;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+`,p=a.Ay.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 1rem;
+  object-fit: contain;
+`,m=a.Ay.h3`
+  font-size: 24px;
+  margin: 1rem 0;
+  color: #18216d;
+`,g=a.Ay.p`
+  font-size: 16px;
+  color: #546274;
+`,b=e=>{let{title:t,exams:r}=e;return(0,l.jsx)(d,{id:"exams",children:(0,l.jsxs)(s.zW,{direction:"up",triggerOnce:!0,children:[(0,l.jsx)(x,{children:t}),(0,l.jsx)(n.A,{gutter:[32,32],justify:"center",children:r.map(((e,t)=>(0,l.jsx)(i.A,{xs:18,sm:10,md:8,lg:6,children:(0,l.jsxs)(h,{to:e.link,children:[(0,l.jsx)(p,{src:(0,c.O)(e.icon),alt:e.title}),(0,l.jsx)(m,{children:e.title}),(0,l.jsx)(g,{children:e.description})]})},t)))})]})})}}}]);
+//# sourceMappingURL=664.2584c7da.chunk.js.map
