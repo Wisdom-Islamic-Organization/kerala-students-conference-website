bc7dd49647a6dd62353bd881e06843ee35e56e24
zameel7 <zameel7@users.noreply.github.com>
2025-02-23 07:31:08 +0000

    deploy: e30c7082897ae5ce16b3bb3d608452833e33385e

---
+++ static/js/745.0101039c.chunk.js
@@ -0,0 +1,113 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[745],{389:(a,e,r)=>{r.d(e,{A:()=>d});var t=r(9281),n=r(9);const i=(0,n.Ay)("p")`
+  margin-top: 1.5rem;
+`,s=(0,n.Ay)("div")`
+  position: relative;
+  max-width: 700px;
+`,o=(0,n.Ay)("div")`
+  border-radius: 3rem;
+  max-width: 400px;
+`;var l=r(579);const d=(0,t.C)()((a=>{let{title:e,content:r,t:t}=a;return(0,l.jsxs)(s,{children:[(0,l.jsx)("h6",{children:t(e)}),(0,l.jsx)(o,{children:(0,l.jsx)(i,{children:t(r)})})]})}))},1745:(a,e,r)=>{r.r(e),r.d(e,{default:()=>C});var t=r(4674),n=r(7419),i=r(1645),s=r(9281),o=r(3614),l=r(7089),d=r(7662),c=r(5639),m=r(389),u=r(3984);const h=JSON.parse('{"Alappuzha":["Naduvathu Nagar","Alappuzha","Kuthiyathodu","Kayamkulam"],"Calicut North":["Balusserry","Poonur","Koyilandy","Payyoli","Vatakara","Nadapuram","Perambra"],"Calicut South":["Atholi","Karapparamb","City","Farook","Koduvalli","Narikuni","Bepoor","Medical College","Thamarasseri","Mankav","Kallai","Mukkam","Nanmanda","Kodiyathoor"],"Ernakulam":["Paravoor","Perumbavoor","Aluva","Kochi","Thripunithura","Kothamangalam","Ernakulam","Palluruthi","Kakkanad"],"Idukki":["Thodupuzha","Adimaly","Peerumedu"],"Kannur":["Valapattanam","Thaliparamb","Irikkoor","Pazhayangadi","Payyanur","Koothuparamb","Iritty","Kannur","Panoor","Thalassery"],"Kasargod":["Manjeshwaram","Kasargode","Kumbala","Uduma","Kanhangad","Trikaripur","SP Nagar","Chauki","Choori","Mogral","Nayanmarmula","Patla"],"Kollam":["Karunagappalli","Pathanapuram","Kollam"],"Kottayam":["Kottayam"],"Malappuram East":["Wandoor","Angaadipuram","Pandikkad","Perintalamnana","Karuvarakund","Edakkara","Aanamangad","Othayi","Moothedam","Edavanna","Nilamboor","Chungathara","Kalikavu","Mampad","Amarambalam"],"Malappuram North":["Mankada","Areekode","Malappuram","Edavannappara","Kavanur","Pulpatta","Cherukavu","Pulikkal","Manjeri","Mongam","Kondotty","Trikkalangode","Anakkayam"],"Malappuram West":["Abdurahman Nager","Kottakkal","University","Vengara","Randathani","Parappanagadi","Paravanna","Tirur","Thiroorangadi","Valavannur","Thanalur","Tanur","Ponnani","Changaramkulam","Maaranjeri","Koottanad","Alathiyoor","Valanjeri","Kuttippuram","Peruvallur"],"Palakkad":["Mannarkkad","Edathanattukara","Alanallur","Ottapalam","Pattambi","Palakkad","Thachambara","Olavakkode","Alathur"],"Pathanamthitta":["Pathanapuram"],"Thrissur":["Pavaratty","Thrissur","Guruvayur","Kaipamangalam","Kunnamkulam","Kodungallur","Chavakkad"],"Thiruvananthapuram":["Trivandrum  East","Trivandrum West","Kaniyapuram","Venjaramood","Attingal"]}');var p=r(9);const g=(0,p.Ay)("div")`
+  padding: 7rem 0 5rem;
+  margin-top: 60px;
+  display: flex;
+  min-height: calc(100vh - 60px);
+
+  @media only screen and (max-width: 1024px) {
+    padding: 5rem 0 3rem;
+  }
+`,x=(0,p.Ay)("div")`
+  height: calc(100vh - 120px);
+  position: relative;
+  padding-right: 2rem;
+
+  @media only screen and (max-width: 1024px) {
+    height: auto;
+    padding-right: 0;
+  }
+`,y=(0,p.Ay)("div")`
+  position: sticky;
+  top: 40%;
+  width: 100%;
+  max-width: 600px;
+  background: white;
+
+  @media only screen and (max-width: 1024px) {
+    position: relative;
+    top: 0;
+    width: 100%;
+  }
+`,A=(0,p.Ay)("form")`
+  width: 100%;
+  max-width: 600px;
+  overflow-y: auto;
+  margin-left: auto;
+
+  @media only screen and (max-width: 1045px) {
+    max-width: 100%;
+    margin-top: 2rem;
+    padding-left: 0;
+  }
+
+  .ant-col {
+    margin-bottom: 1rem;
+  }
+`,v=(0,p.Ay)("span")`
+  display: block;
+  font-weight: 600;
+  color: rgb(36, 182, 200);
+  height: 0.775rem;
+  padding: 0 0.675rem;
+`,b=(0,p.Ay)("div")`
+  text-align: center;
+  position: relative;
+  margin-top: 2rem;
+
+  button {
+    border: 2px solid white;
+  }
+
+  @media only screen and (max-width: 414px) {
+    padding-top: 0.75rem;
+  }
+`,j=(0,p.Ay)("label")`
+  display: block;
+  margin-bottom: 6px;
+  margin-top: 15px;
+  font-size: 14px;
+  font-weight: 500;
+  color: #000;
+`,k=(0,p.Ay)(t.A)`
+  width: 100%;
+  margin-top: 5px;
+  
+  .ant-select-selector {
+    border: 1px solid #dadada !important;
+    border-radius: 4px !important;
+    padding: 8px 12px !important;
+    height: auto !important;
+    
+    &:hover, &:focus {
+      border-color: rgb(0, 0, 0) !important;
+    }
+  }
+`;var P=r(579);const{Option:f}=t.A,C=(0,s.C)()((a=>{var e;let{title:r,content:t,id:s,t:p}=a;const{values:C,errors:E,handleChange:N,handleSubmit:_}=(0,l.m)(d.A,"registration"),S=a=>{N(a)},w=a=>{let{type:e}=a;const r=E[e];return(0,P.jsx)(v,{children:r})},T=a=>1===a?"st":2===a?"nd":3===a?"rd":"th",K=[...Object.keys(h),"Others"];return(0,P.jsx)(g,{id:s,children:(0,P.jsxs)(n.A,{justify:"space-between",align:"top",children:[(0,P.jsx)(i.A,{lg:12,md:11,sm:24,xs:24,children:(0,P.jsx)(x,{children:(0,P.jsx)(y,{children:(0,P.jsx)(o.q7,{direction:"left",triggerOnce:!0,children:(0,P.jsx)(m.A,{title:r,content:t})})})})}),(0,P.jsx)(i.A,{lg:12,md:12,sm:24,xs:24,children:(0,P.jsx)(o.q7,{direction:"right",triggerOnce:!0,children:(0,P.jsxs)(A,{autoComplete:"off",onSubmit:_,children:[(0,P.jsxs)(i.A,{span:24,children:[(0,P.jsx)(u.A,{type:"text",name:"name",placeholder:"Full Name",value:C.name||"",onChange:S}),(0,P.jsx)(w,{type:"name"})]}),(0,P.jsxs)(i.A,{span:24,children:[(0,P.jsx)(j,{children:"Gender"}),(0,P.jsxs)(k,{placeholder:"Select Gender",onChange:a=>S({target:{name:"gender",value:a}}),children:[(0,P.jsx)(f,{value:"male",children:"Male"}),(0,P.jsx)(f,{value:"female",children:"Female"})]}),(0,P.jsx)(w,{type:"gender"})]}),(0,P.jsxs)(n.A,{gutter:16,children:[(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(u.A,{type:"number",name:"age",placeholder:"Age",value:C.age||"",onChange:S}),(0,P.jsx)(w,{type:"age"})]}),(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(j,{children:"Standard/Course"}),(0,P.jsx)(k,{placeholder:"Standard/Course",onChange:a=>S({target:{name:"standard",value:a}}),value:C.standard||void 0,children:["High School","Higher Secondary","UG","PG","MPhil","PhD","Others"].map((a=>(0,P.jsx)(f,{value:a,children:a},a)))})]})]}),(0,P.jsx)(i.A,{span:24,children:(0,P.jsx)(u.A,{type:"text",name:"subject",placeholder:"Subject",value:C.subject||"",onChange:S})}),(0,P.jsx)(i.A,{span:24,children:(0,P.jsx)(u.A,{type:"text",name:"institution",placeholder:"Institution",value:C.institution||"",onChange:S})}),C.standard&&(O=C.standard,["UG","PG","MPhil","PhD","Others"].includes(O))&&(0,P.jsxs)(n.A,{gutter:16,children:[(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(j,{children:"Year of Study"}),(0,P.jsx)(k,{placeholder:"Year of Study",onChange:a=>S({target:{name:"yearOfStudy",value:a}}),value:C.yearOfStudy||void 0,children:[1,2,3,4,5].map((a=>(0,P.jsx)(f,{value:a,children:`${a}${T(a)} Year`},a)))})]}),(0,P.jsx)(i.A,{span:12,children:(0,P.jsx)(u.A,{type:"text",name:"yearOfCompletion",placeholder:"Year of Completion",value:C.yearOfCompletion||"",onChange:S})})]}),(0,P.jsxs)(n.A,{gutter:16,children:[(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(j,{children:"District"}),(0,P.jsx)(k,{placeholder:"Select District",onChange:a=>S({target:{name:"district",value:a}}),value:C.district||void 0,children:K.map((a=>(0,P.jsx)(f,{value:a,children:a},a)))}),(0,P.jsx)(w,{type:"district"})]}),(0,P.jsx)(i.A,{span:12,children:"Others"===C.district?(0,P.jsx)("div",{children:(0,P.jsx)(u.A,{type:"text",name:"otherDistrict",placeholder:"Enter District Name",value:C.otherDistrict||"",onChange:S})}):(0,P.jsxs)(P.Fragment,{children:[(0,P.jsx)(j,{children:"Local Body"}),(0,P.jsx)(k,{placeholder:"Select Local Body",onChange:a=>S({target:{name:"localBody",value:a}}),value:C.localBody||void 0,children:C.district&&(null===(e=h[C.district])||void 0===e?void 0:e.map((a=>(0,P.jsx)(f,{value:a,children:a},a))))})]})})]}),(0,P.jsxs)(n.A,{gutter:16,children:[(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(u.A,{type:"tel",name:"contactNumber",placeholder:"Contact Number",value:C.contactNumber||"",onChange:S}),(0,P.jsx)(w,{type:"contactNumber"})]}),(0,P.jsxs)(i.A,{span:12,children:[(0,P.jsx)(j,{children:"Is WhatsApp number different?"}),(0,P.jsxs)(k,{placeholder:"Select Yes/No",onChange:a=>S({target:{name:"isDifferentWhatsApp",value:a}}),value:C.isDifferentWhatsApp||void 0,children:[(0,P.jsx)(f,{value:"no",children:"No"}),(0,P.jsx)(f,{value:"yes",children:"Yes"})]})]})]}),"yes"===C.isDifferentWhatsApp&&(0,P.jsxs)(i.A,{span:24,children:[(0,P.jsx)(u.A,{type:"tel",name:"whatsappNumber",placeholder:"WhatsApp Number",value:C.whatsappNumber||"",onChange:S}),"yes"===C.isDifferentWhatsApp&&(0,P.jsx)(w,{type:"whatsappNumber"})]}),(0,P.jsxs)(i.A,{span:24,children:[(0,P.jsx)(u.A,{type:"email",name:"email",placeholder:"Email Address",value:C.email||"",onChange:S}),(0,P.jsx)(w,{type:"email"})]}),(0,P.jsx)(b,{children:(0,P.jsx)(c.$,{name:"submit",children:p("Register")})})]})})})]})});var O}))},3984:(a,e,r)=>{r.d(e,{A:()=>d});var t=r(9281),n=r(9);const i=(0,n.Ay)("div")`
+  display: inline-block;
+  width: 100%;
+  padding: 10px 5px;
+`,s=(0,n.Ay)("input")`
+  font-size: 0.875rem;
+`,o=((0,n.Ay)("div")`
+  display: inline-block;
+  width: 100%;
+  padding: 10px 5px;
+  margin-bottom: -0.625rem;
+`,(0,n.Ay)("textarea")`
+  resize: none;
+  font-size: 0.875rem;
+  height: 185px;
+`,(0,n.Ay)("label")`
+  display: block;
+  padding-bottom: 10px;
+  text-transform: capitalize;
+`);var l=r(579);const d=(0,t.C)()((a=>{let{name:e,placeholder:r,onChange:t,t:n}=a;return(0,l.jsxs)(i,{children:[(0,l.jsx)(o,{htmlFor:e,children:n(r)}),(0,l.jsx)(s,{placeholder:n(r),name:e,id:e,onChange:t})]})}))},7089:(a,e,r)=>{r.d(e,{m:()=>m});var t=r(5043),n=r(6904),i=r(2387),s=r(5472),o=r(1688);const l={NODE_ENV:"production",PUBLIC_URL:"",WDS_SOCKET_HOST:void 0,WDS_SOCKET_PATH:void 0,WDS_SOCKET_PORT:void 0,FAST_REFRESH:!0,REACT_APP_FIREBASE_MESSAGING_SENDER_ID:"93730832178",REACT_APP_FIREBASE_MEASUREMENT_ID:"G-43VT1LQE97",REACT_APP_FIREBASE_STORAGE_BUCKET:"kerala-students-conference.firebasestorage.app",REACT_APP_FIREBASE_AUTH_DOMAIN:"kerala-students-conference.firebaseapp.com",REACT_APP_FIREBASE_PROJECT_ID:"kerala-students-conference",REACT_APP_FIREBASE_APP_ID:"1:93730832178:web:e2cba01e8d356201fde12b",REACT_APP_FIREBASE_API_KEY:"AIzaSyAsFWymdJpYHMHcHgDk1_h4xEkDxXAvF1A"}.REACT_APP_GUIDE_API_TOKEN,d={name:"",email:"",age:"",standard:"",gender:"",subject:"",institution:"",yearOfStudy:"",yearOfCompletion:"",district:"",otherDistrict:"",localBody:"",contactNumber:"",whatsappNumber:"",isDifferentWhatsApp:"no"},c={name:"",contactNumber:""},m=function(a){let e=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"registration";const r=(0,o.W6)(),[m,u]=(0,t.useState)({values:"registration"===e?d:c,errors:"registration"===e?d:c});return{handleChange:a=>{const{name:e,value:r}=a.target;u((a=>({...a,values:{...a.values,[e]:r},errors:{...a.errors,[e]:""}})))},handleSubmit:async t=>{t.preventDefault();const o=m.values,h=a(o);u((a=>({...a,errors:h})));try{if(Object.values(h).every((a=>""===a))){const a=(0,s.rJ)(i.db,"registrations");await(0,s.gS)(a,{...o,timestamp:(0,s.O5)()}),"registration"===e&&await(async a=>{try{const e=await fetch("https://guideportal.wisdomislam.org/api/p/event-registration",{method:"POST",headers:{"Content-Type":"application/json",Authorization:`Bearer ${l}`},body:JSON.stringify(a)});if(!e.ok)throw new Error("API submission failed");return await e.json()}catch(e){throw console.error("API Error:",e),e}})({event_id:84,mobile:o.contactNumber||"",name:o.name||"",age:o.age||"",sex:"male"===o.gender?"Male":"Female",whatsapp:o.whatsappNumber||o.contactNumber||"",email:o.email||"",class:o.standard||"",subject:o.subject||"",institution:o.institution||"",year_of_study:o.yearOfStudy||"",year_of_completion:o.yearOfCompletion||"",district_name:o.district||"",district_name_other:o.otherDistrict||"",local_body:o.localBody||""}),u({values:"registration"===e?{...d}:{...c},errors:"registration"===e?{...d}:{...c}}),window.scrollTo({top:0,behavior:"smooth"}),setTimeout((()=>{n.A.success({message:"Registration Successful",description:"Thank you for registering! We'll contact you soon.",duration:3})}),500),setTimeout((()=>{r.push("/")}),2500)}}catch(p){console.error("Registration error:",p),n.A.error({message:"Error",description:"Failed to submit registration. Please try again later."})}},handleContact:async e=>{e.preventDefault();const r=m.values,t=a(r);u((a=>({...a,errors:t})));try{if(Object.values(t).every((a=>""===a))){const a=(0,s.rJ)(i.db,"contacts");await(0,s.gS)(a,{name:r.name,contactNumber:r.contactNumber,timestamp:(0,s.O5)()}),u({values:{...c},errors:{...c}}),n.A.success({message:"Message Sent",description:"Thank you for contacting us! We'll get back to you soon.",duration:3})}}catch(o){console.error("Contact form error:",o),n.A.error({message:"Error",description:"Failed to send message. Please try again later."})}},values:m.values,errors:m.errors}}},7662:(a,e,r)=>{r.d(e,{A:()=>n,D:()=>t});const t=a=>{let e={};return a.name||(e.name="Name is required"),a.contactNumber||(e.contactNumber="Contact number is required"),e},n=a=>{let e={};return a.name||(e.name="Name is required"),a.gender||(e.gender="Gender is required"),a.age?(isNaN(Number(a.age))||Number(a.age)<0)&&(e.age="Please enter a valid age"):e.age="Age is required",a.email?/\S+@\S+\.\S+/.test(a.email)||(e.email="Email address is invalid"):e.email="Email address is required",a.contactNumber?/^\d{10}$/.test(a.contactNumber)||(e.contactNumber="Please enter a valid 10-digit number"):e.contactNumber="Contact number is required",a.whatsappNumber&&!/^\d{10}$/.test(a.whatsappNumber)&&(e.whatsappNumber="Please enter a valid 10-digit number"),e}}}]);
+//# sourceMappingURL=745.0101039c.chunk.js.map
